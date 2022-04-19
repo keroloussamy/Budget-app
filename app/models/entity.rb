552049444720236351ca03e7e553c42c85bb0d0e
kerolous samy <keroloussamy98@gmail.com>
2022-04-19 23:49:32 +0200

    Update entity validation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,5 +3,5 @@ class Entity < ApplicationRecord
   has_and_belongs_to_many :groups
 
   validates :name, presence: true
-  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, numericality: true
 end
