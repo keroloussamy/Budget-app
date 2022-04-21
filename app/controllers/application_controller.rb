a7cdd07279176a8980c6e60c355200a11632f227
keroloussamy <keroloussamy98@gmail.com>
2022-04-21 03:41:24 +0200

    Change route after sign up and sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
+
+    def after_sign_in_path_for(resource)
+      groups_path
+    end
+
+    def after_sign_up_path_for(resource)
+      groups_path
+    end
+        
 end
