98c435163822e6d433b324366697de39bc5f9db6
keroloussamy <keroloussamy98@gmail.com>
2022-04-22 18:16:41 +0200

    Fix routes issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :groups, only: [:create, :index, :show]
-  resources :entities, only: [:create, :index, :show]
-  devise_for :users, only: [:create, :index, :show]
+  resources :groups, only: [:new, :create, :index, :show]
+  resources :entities, only: [:new, :create, :index, :show]
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
